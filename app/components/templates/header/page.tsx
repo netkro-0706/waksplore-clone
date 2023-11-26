11e4df94b03ee707aada84b7364ef28ea39c5c46
akira mac <kroging@gmail.com>
2023-11-27 02:08:52 +0900

    first set

---
+++ app/components/templates/header/page.tsx
@@ -0,0 +1,13 @@
+import Button from "~/components/primitives/navbar/button/page";
+
+export default function HeaderTemplate() {
+  return (
+    <div className="">
+      <Button text={"Home"} href={"/"} />
+
+      <>template Icon</>
+      <>feature searchBar</>
+      <></>
+    </div>
+  );
+}
