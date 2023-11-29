479fc85ebd76cd198ee454c6113ca2cf2a540d86
akira mac <kroging@gmail.com>
2023-11-29 19:53:13 +0900

    fix: add constants of SEARCH icon size

--- app/components/primitives/_asset/constants/page.ts
+++ app/components/primitives/_asset/constants/page.ts
@@ -1,5 +1,6 @@
 export const ICON_SIZE = {
   LOGO: { width: 32, height: 32 },
   CAFE: { width: 30, height: 30 },
+  SEARCH: { width: 20, height: 20 },
   UNION_LOGO: { width: 125, height: 125 },
 } as const;
