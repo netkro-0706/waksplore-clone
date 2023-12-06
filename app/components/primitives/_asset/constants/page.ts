e684810ed1f387ece5396b6cb1e83ad2f239a2ec
netkro <kroging@gmail.com>
2023-12-06 16:05:13 +0900

    fix: add ICON SIZE's constants

--- app/components/primitives/_asset/constants/page.ts
+++ app/components/primitives/_asset/constants/page.ts
@@ -1,4 +1,6 @@
 export const ICON_SIZE = {
+  MAGAZINE_ARROW: { width: 16, height: 16 },
+  NOTIFICATION: { width: 16, height: 16 },
   LOGO: { width: 32, height: 32 },
   CAFE: { width: 30, height: 30 },
   SEARCH: { width: 20, height: 20 },
